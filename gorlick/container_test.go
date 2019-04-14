3166397e7b292a7f9a01464e30b81096f6dbca9b
uncleglitch <uncleglitch@ya.ru>
2019-04-14 22:16:02 +0200

    Products and Containers have been added.

---
+++ gorlick/container_test.go
@@ -0,0 +1,23 @@
+package gorlick_test
+
+import (
+	"reflect"
+	"testing"
+
+	g "github.com/uncleglitch/gorlick/gorlick"
+)
+
+func TestMakeContainer(t *testing.T) {
+	name := "container1"
+	containerType := g.STANDARD
+	p1 := g.MakeProduct(0, "Product0", g.THING, "Description of Product0")
+	p2 := g.MakeProduct(1, "Product1", g.LITER, "Description of Product1")
+	products := make([]g.Product, 2)
+	products = append(products, p1)
+	products = append(products, p2)
+
+	c := g.MakeContainer(name, containerType, products)
+	if c.Name != name || c.Type != containerType || !reflect.DeepEqual(c.Products, products) {
+		t.Fail()
+	}
+}
---
+++ gorlick/gorlick_test.go
@@ -0,0 +1,1 @@
+package gorlick_test
