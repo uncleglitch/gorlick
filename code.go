192479fbab806474c7564d0370b6f744c68927c9
uncleglitch <uncleglitch@ya.ru>
2019-06-06 19:20:17 +0200

    Some new files have been added.

---
+++ code.go
@@ -0,0 +1,11 @@
+package gorlick
+
+/// Parse parses code and makes a scenario.
+// func Parse(code string) (*Scenario, error) {
+
+// }
+
+/// Generate makes code for the scenario.
+// func Generate(s Scenario) (string, error) {
+// 	return "", nil
+// }
---
+++ code_test.go
@@ -0,0 +1,13 @@
+package gorlick_test
+
+// import (
+// 	"testing"
+// )
+
+// func TestParse(t *testing.T) {
+
+// }
+
+// func TestGenerate(t *testing.T) {
+
+// }
